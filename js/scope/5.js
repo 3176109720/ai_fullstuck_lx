0730d85a59a26ad126a0a464faad73c953267d69
3176109720 <3176109720@qq.com>
2023-10-27 22:38:50 +0800

    作用域

---
+++ js/scope/5.js
@@ -0,0 +1,46 @@
+// function foo(str) {
+//     eval(str)  // var b = 2
+//     var a = 1
+
+//     console.log(a,b);
+// }
+// foo('var b = 2'); 
+
+
+var obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+}
+
+// obj.a = 2
+// obj.b = 3
+// obj.c = 4
+
+// with(obj){    // 批量修改obj内的值
+//     a = 2,
+//     b = 3.
+//     c = 4
+// }
+
+// console.log(obj);
+
+
+function foo(obj) {
+    with (obj) {
+        a = 2
+    }
+}
+
+var o1 = {
+    a: 3
+}
+var o2 = {
+    b: 4
+}
+
+foo(o2)
+console.log(a);
+
+
+c = 3  //也算变量声明
