a375167a28f9ee2b6142417e10a87919d1784ccf
3176109720 <3176109720@qq.com>
2023-11-15 21:32:57 +0800

    原型

---
+++ js/原型/3.js
@@ -0,0 +1,17 @@
+Car.prototype = {
+    name: 'BMW',
+    long: 4900,
+    height: 1400
+  }
+  function Car(owner, color) {
+    this.owner = owner
+    this.color = color
+  }
+
+  let car = new Car('李总', 'red')
+  let car2 = new Car('戴总', 'pink')
+  
+//   car.name = '奔驰'
+delete Car.prototype.name
+
+  console.log(car);
