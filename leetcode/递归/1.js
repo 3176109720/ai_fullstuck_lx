19ae19d9a1ee453daa0c7119c20e1f99f3448033
3176109720 <3176109720@qq.com>
2023-11-15 17:41:52 +0800

    递归

---
+++ leetcode/递归/1.js
@@ -0,0 +1,19 @@
+// 数组降维扁平化
+var arr = [1, [2, [3.4]]]//     ->   [1.2.3.4]
+
+// for (var i = 0; i < arr.length; i++) {
+//     let res = []
+//     if (!Array.isArray(arr[i])) {
+//         res.push(arr[i])
+//     }else{
+//         for (var j = 0; j < item.length; j++) {
+//             if (!Array.isArray(item[j])) {
+//                 res.push(arr[i])
+//             }else{}
+//         }
+//     }
+// }
+
+var newArr = arr.flat(Infinity)  // Infinity : 无穷大
+
+console.log(newArr);
---
+++ leetcode/递归/2.js
@@ -0,0 +1,30 @@
+// 5!
+function mul(n) {
+// function mul(n) {
+//     let res = 1
+//     for (var i = 1; i <= n; i++) {
+//         res = res * i
+//     }
+//     return res
+// }
+// mul(5)//120
+// console.log(mul)
+  if(n<=1)
+{
+    return 1
+}
+  return n * mul(n - 1)
+
+}
+
+console.log(mul(5));  //  120
+
+// mul(5) == 5 * mul(4)
+// mul(4) == 4 * mul(3)
+// mul(3) == 5 * mul(2)
+// mul(2) == 4 * mul(1)
+// mul(1) == 1
+
+
+// 找出口
+// 找入口
