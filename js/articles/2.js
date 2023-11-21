6b99b0aaf8e962bdf5e33b1fe085f3611eb8fa83
3176109720 <3176109720@qq.com>
2023-11-21 17:02:16 +0800

    数组字符串方法

---
+++ js/articles/2.js
@@ -0,0 +1,9 @@
+// 如何去除 The A An   
+function strip(bandName) {
+
+}
+
+strip ('The Plot in You') // Plot in You
+strip ('A Skylit Drive') // Skylit Drive
+strip ('An old Dog') // old Dog
+strip('We Came as Romans') // We Came as Romans 
