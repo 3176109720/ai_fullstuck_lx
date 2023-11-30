3563a90ef8be49394331750a0ce273bc12e56d12
3176109720 <3176109720@qq.com>
2023-11-30 23:02:14 +0800

    动态规划杨辉三角

---
+++ leetcode/118/1.js
@@ -0,0 +1,10 @@
+var generate = function(numRows) {
+    let dp = [];
+       for (let i = 0; i < numRows; i++) {
+           dp[i] = Array(i+1).fill(1);
+           for (let j = 1; j < i; j++) {
+               dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+           }
+       }
+       return dp;
+}
