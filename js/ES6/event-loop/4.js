f561f307f2e249a5ec38c7c58dff1f0948344353
3176109720 <3176109720@qq.com>
2023-12-02 16:11:30 +0800

    进程 线程 异步 Event-Loop

---
+++ js/ES6/event-loop/4.js
@@ -0,0 +1,30 @@
+async function fn1() {
+    await fn2()
+    await fn3()
+    console.log('fn1 end');
+  }
+  fn1()
+  async function fn2() {
+    console.log('fn2 end');
+  }
+  async function fn3() {
+    console.log('fn3 end');
+  }
+  
+  setTimeout(() => {
+    new Promise((resolve) => {
+      console.log('setTimeout');
+      resolve()
+    })
+    .then(() => {
+      console.log('then');
+    })
+    .then(() => {
+      setTimeout(() => {
+        console.log('then2 end');
+      })
+    })
+    console.log('setTimeout end');
+  }) 
+
+  //fn2  fn3  fn1   set   set end    then   then2 
