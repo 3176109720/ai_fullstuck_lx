4e2e4b89aa5802bd6b6d6170df4c59de3c11269d
3176109720 <3176109720@qq.com>
2023-12-15 21:42:35 +0800

    vue-router

---
+++ vue/learn_vue/learn-vue-router/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home">
+        <h2>这是首页</h2>
+        <nav>
+            <router-link to="suggest">推荐</router-link>
+            <router-link to="/home/newest">最新</router-link>
+        </nav>
+        
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+nav a{
+    margin-left: 20px;
+
+}
+</style>
