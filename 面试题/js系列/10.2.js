e1525b1bfb4b6ae08eea284ab0d1596558d5313a
3176109720 <3176109720@qq.com>
2024-03-05 21:36:22 +0800

    this 的绑定规则拓展

---
+++ 面试题/js系列/10.2.js
@@ -0,0 +1,13 @@
+Parent.prototype.say = 'hello'
+function Parent() {
+  this.name = 'parent'
+}
+
+function Child() {
+  Parent.call(this)      // this.name = 'parent'
+  this.type = 'child'
+}
+
+let c = new Child()
+
+console.log(c.say);
